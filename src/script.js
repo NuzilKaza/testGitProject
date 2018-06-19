f1d1219d84bb414d3193292674376237b4bdd4c3
Elizaveta Kazankova <eliz.cazankova@gmail.com>
2018-06-19 13:42:07 +0600

    bye

--- src/script.js
+++ src/script.js
@@ -2,3 +2,8 @@ function hello() {
     var p = document.getElementsByTagName('p')[0];
     p.innerHTML = 'Hello';
 }
+
+function bye() {
+    var p = document.getElementsByTagName('p')[0];
+    p.innerHTML = 'Bye';
+}
